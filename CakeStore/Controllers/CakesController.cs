1877d1e8a25a9d2d7db289cacfdcc76b5c5f96fe
Truongvvd00423 <truongvvd00423@fpt.edu.vn>
2018-06-06 06:50:04 +0700

    asignment web app

---
+++ CakeStore/Controllers/CakesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CakeStore.Models;
+
+namespace CakeStore.Controllers
+{
+    public class CakesController : Controller
+    {
+        private CakeContext db = new CakeContext();
+
+        // GET: Cakes
+        public ActionResult Index()
+        {
+            var cakes = db.Cakes.Include(c => c.Type);
+            return View(cakes.ToList());
+        }
+
+        // GET: Cakes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cake cake = db.Cakes.Find(id);
+            if (cake == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cake);
+        }
+
+        // GET: Cakes/Create
+        public ActionResult Create()
+        {
+            ViewBag.typeId = new SelectList(db.Types, "id", "name");
+            return View();
+        }
+
+        // POST: Cakes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,typeId,name,image,detail,price")] Cake cake)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cakes.Add(cake);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.typeId = new SelectList(db.Types, "id", "name", cake.typeId);
+            return View(cake);
+        }
+
+        // GET: Cakes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cake cake = db.Cakes.Find(id);
+            if (cake == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.typeId = new SelectList(db.Types, "id", "name", cake.typeId);
+            return View(cake);
+        }
+
+        // POST: Cakes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,typeId,name,image,detail,price")] Cake cake)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cake).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.typeId = new SelectList(db.Types, "id", "name", cake.typeId);
+            return View(cake);
+        }
+
+        // GET: Cakes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cake cake = db.Cakes.Find(id);
+            if (cake == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cake);
+        }
+
+        // POST: Cakes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cake cake = db.Cakes.Find(id);
+            db.Cakes.Remove(cake);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CakeStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CakeStore.Startup))]
+namespace CakeStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
